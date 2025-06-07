40ec3fa583b1b21aa627eedecbbe8d9cb3592be5
QuikApp <prasannasrinivasan32@gmail.com>
2025-06-07 14:40:11 +0530

    first commit 03

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.quikapp4"
+    namespace = "${System.getenv("BUNDLE_ID") ?: "com.example.app"}"
     compileSdk = 35
     ndkVersion = "27.0.12077973"
 
@@ -22,7 +22,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.quikapp4"
+        applicationId = "${System.getenv("BUNDLE_ID") ?: "com.example.app"}"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 21
